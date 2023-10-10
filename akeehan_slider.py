c71c8a7c5d4e296fe2bfede170a4be7be928f01a
Alex <alexk2678@yahoo.com>
2023-10-10 13:30:44 -0400

    Got insert into frontier working kinda. Trying to match up the frontier with greedy currently.

--- akeehan_slider.py
+++ akeehan_slider.py
@@ -14,17 +14,6 @@ def solve(p):
     '''Finds a sequence of moves ("L", "U", "R", or "D") that will solve the
     Puzzle object passed. Returns that sequence in a list.
     '''
-    # Define what a state of the board is
-    class State:
-        def __init__(self, state, parent=None, cost=0, heuristic=0):
-            self.state = state
-            self.parent = parent
-            self.cost = cost
-            self.heuristic = heuristic
-
-    # Get neighbors
-    def get_neighbors():
-        return p.legal_moves()
 
     # Calculate distance
     def calculate_distance(node, puzzle, solved):
@@ -63,19 +52,53 @@ def solve(p):
         
         return total_dist
 
+    def insert_frontier(frontier, frontierEstimates, frontierPuzzles, state, heuristic):
+        for x in range(len(frontier)):
+            #frontier_heuristic = get_heuristic(x)
+            #if heuristic > frontier_heuristic:
+            if frontierEstimates[frontier[x]] > heuristic:
+                frontier.insert(x, state)
+                frontierEstimates[state] = heuristic
+                frontierPuzzles[state] = deepcopy(frontierPuzzles[frontier[x - 1]])
+                return
+        frontier.append(state)
+        frontierEstimates[state] = heuristic
+        frontierPuzzles[state] = deepcopy(frontierPuzzles[frontier[-1]])
 
 
+    # Do greedy Search
+    def greedy(p):
+        frontier = [()]
+        frontierEstimates = {() : get_heuristic(p)}
+        frontierPuzzles = {() : p}
+        
+        while frontier:
+            curr_state = frontier.pop(0)
+            #curr_heuristic = frontierEstimates[curr_state]
+            curr_puzzle = frontierPuzzles[curr_state]
 
-    # Do A* Search
-    def astar ():
-        return 0
+            if get_heuristic(curr_puzzle) == 0:
+                return curr_state
+            
+            legal_moves = curr_puzzle.legal_moves()
+            for move in legal_moves:
+                puzzle = deepcopy(curr_puzzle)
+                puzzle.move(move)
+                new_state = curr_state + (move,)
+                #heuristic = curr_heuristic + 1
+                heuristic = get_heuristic(puzzle)
+
+                if new_state not in frontierEstimates or heuristic < frontierEstimates[new_state]:
+                    insert_frontier(frontier, frontierEstimates, frontierPuzzles, new_state, heuristic)
+
     
-
- 
-    heuristic = get_heuristic(p)
-    print("Result", result)
+    ans = greedy(p)
+    print("NICE")
+    print("ans", ans)
+    #heuristic = get_heuristic(p)
+    #print("Result", heuristic)
     # Here's a (bogus) example return value:
-    return ["D","U","L","L"]
+    #return ["D","U","L","L"]
 
 
 
